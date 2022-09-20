2ca331179b21c0518e641d2544cfa3193048ab76
konalhamdi <2012138017hmdknl@gmail.com>
2022-09-20 22:28:13 +0300

    firstcommit

--- ödev13/src/intro/main.java
+++ ödev13/src/intro/main.java
@@ -5,7 +5,22 @@ public class main {
 	public static void main(String[] args) {
 		char harf='A';
 		
-		
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case'O':
+		case'U':
+			System.out.println("Kalın Sesli Harf");
+			break;
+		case'E':
+		case'İ':
+		case'Ü':
+		case'Ö':
+			System.out.println("İnce Sesli Harf");
+			break;
+			default:
+				System.out.println("Lütfen Sesli Harf Giriniz");
+		}
 
 	}
 
---
+++ ödev14/src/intro/main.java
@@ -0,0 +1,20 @@
+package intro;
+
+public class main {
+
+	public static void main(String[] args) {
+		int number = 193;
+		int total = 0;
+		for (int i = 1; i < number; i++) {
+			if (number % i == 0) {
+				total = total + i;
+			}
+		}
+		if (total == number) {
+			System.out.println("Mükemmel Sayıdır");
+		} else {
+			System.out.println("Mükemmel Sayı Değildir");
+		}
+	}
+
+}
